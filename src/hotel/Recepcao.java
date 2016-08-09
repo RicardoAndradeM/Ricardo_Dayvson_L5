bfc6ee26894602789c5afa3c5be55f51f2f43e10
Ricardo Andrade <ricardo.maia12@gmail.com>
2016-08-09 09:14:21 -0300

    Classe Recepcao criada

---
+++ src/hotel/Recepcao.java
@@ -0,0 +1,35 @@
+package hotel;
+
+import java.util.ArrayList;
+
+public class Recepcao {
+	
+	private ArrayList<Estadia> estadias;
+	
+	public Recepcao() {
+		this.estadias = new ArrayList<Estadia>();
+	}
+	
+	public void checkin(String nome, String tipo, int idade, int dias, double valor) {
+		
+
+	}
+	
+	public void checkout(String nome){
+		
+	}
+	
+	public int getNumDeHospedes() {
+		
+	}
+	
+	public double getLucroTotal() {
+		
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+}
