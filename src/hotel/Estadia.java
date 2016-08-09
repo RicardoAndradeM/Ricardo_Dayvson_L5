4c98f233461a59cdc4a1b58dab9f271b93343f26
DayvsonNascimento <dayvson.nascimento@ccc.ufcg.edu.br>
2016-08-09 09:25:46 -0300

    Criei Estadia

---
+++ src/hotel/Estadia.java
@@ -0,0 +1,43 @@
+package hotel;
+
+public class Estadia {
+	Animal animal;
+	double valorQuarto;
+	int qtdDias;
+
+	public Estadia(String nome, String tipo, int idade, int qtdDias, double valorQuarto) throws Exception{
+		animal = new Animal( nome,  tipo,  idade);
+		this.valorQuarto = valorQuarto;
+		this.qtdDias = qtdDias;
+	}
+
+	
+	public String getNome() {
+		return animal.getNome();
+	}
+
+	public String getTipo() {
+		return animal.getTipo();
+
+	}
+
+	public int getIdade() {
+		return animal.getIdade();
+	}
+
+	public double getValorQuarto() {
+		return valorQuarto;
+	}
+
+	public int getQtdDias() {
+		return qtdDias;
+	}
+
+
+	@Override
+	public String toString() {
+		return getNome() + ", " + getTipo() + ", " + getQtdDias() + 
+				" dias com o preço de R$ " + String.format("%.2f", getValorQuarto());
+
+	}
+}	
