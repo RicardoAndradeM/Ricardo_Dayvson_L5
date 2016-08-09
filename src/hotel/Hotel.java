aeab71f05c489a3a83e275fd01630643a100df4c
Ricardo Andrade <ricardo@Ricardos-Mac.local>
2016-08-09 18:07:51 -0300

    Classe Hotel finalizada

--- src/hotel/Hotel.java
+++ src/hotel/Hotel.java
@@ -7,7 +7,24 @@ public class Hotel {
 		this.recepcao = new Recepcao();
 	}
 	
-	public void checkin(String nome) {
-		
+	public void checkin(String nome, string tipo, int idade, int dias, double valor) {
+		this.recepcao.checkin(nome, tipo, idade, dias, valor);
+	}
+	
+	public void checkout(String nome) {
+		this.recepcao.checkout(nome);
+	}
+	
+	public int getNumDeHospedes() {
+		return this.recepcao.getNumDeHospedes();
+	}
+	
+	public double getLucroTotal() {
+		return this.recepcao.getLucroTotal();
+	}
+	
+	@Override
+	public String toString() {
+		return this.recepcao.toString();
 	}
 }
